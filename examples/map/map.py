c6973ebeebf5705c6790d5d678949f884f724332
Darius Foo <darius.foo.tw@gmail.com>
2019-11-19 00:17:34 +0800

    Use APIs better

--- examples/map/map.py
+++ examples/map/map.py
@@ -2,17 +2,11 @@
 
 from exnihilo import *
 from graphviz import Digraph
-import subprocess
 
 world = solve(file='map.lp')
 g = Digraph()
 
-for wo in world:
-  [g.node(str(v.id), str(v.id)) for v in wo['vertex'].itertuples()]
-  [g.edge(str(e.a), str(e.b)) for e in wo['edge'].itertuples()]
-
-  with open('/tmp/lol.dot', 'w') as f:
-    f.write(g.source)
-
-  subprocess.check_call(['dot', '-o', '/tmp/lol.png', '-Tpng', '/tmp/lol.dot'])
-  subprocess.check_call(['open', '/tmp/lol.png'])
+for w in world:
+  [g.node(str(v.id), str(v.id)) for v in w['vertex'].itertuples()]
+  [g.edge(str(e.a), str(e.b)) for e in w['edge'].itertuples()]
+  g.render(view=True, directory='/tmp')
