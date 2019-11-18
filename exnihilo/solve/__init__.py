cbfa94af80fafe70056ea99f507a315b071c8425
Darius Foo <darius.foo.tw@gmail.com>
2019-11-19 00:09:09 +0800

    Cleanup

--- exnihilo/solve/__init__.py
+++ exnihilo/solve/__init__.py
@@ -5,14 +5,6 @@ import pandas as pd
 from .interruptible import *
 import re
 
-program = '''
-a.
-{ b(1) }.
-b(2).
-c(X) :- b(X).
-'''
-
-# [frozenset({('c', (2,)), ('b', (1,)), ('a', ())})]
 
 def fst(x):
   return x[0]
@@ -25,6 +17,7 @@ def process(answer):
 def process_df(answer, mappings):
   g = itertools.groupby(sorted(answer, key=fst), key=fst)
   return {k: pd.DataFrame((name_and_args[1] for name_and_args in v), columns=mappings.get(k, [])) for k, v in g}
+
 
 def solve(program=None, file=None):
   if not program and not file:
@@ -44,20 +37,9 @@ def solve(program=None, file=None):
   # TODO handle unsat
   # TODO random
   # TODO number of answers
-  # TODO file vs str input
-  # TODO return a dataframe
-
-  # TODO indexing relations, or maybe querying dataframe is sufficient
-
-  # TODO translation into records with names taken from annotations
-  # TODO automatically define the records
-  # TODO examples and tests
 
   answers = clyngor.ASP(program).careful_parsing
   # assumes that there's no overloading by arity
 
   for answer in answers: # nondeterminism
     yield process_df(answer, mappings)
-
-# for a in solve(program):
-  # print(a)
--- readme.md
+++ readme.md
@@ -1,8 +1,22 @@
 
+# Ex Nihilo
+
 ## Installation
 
 ```sh
 pip install git+https://github.com/dariusf/exnihilo.git
+brew install graphviz clingo
+```
+
+## Getting started
+
+The `exnihilo` executable is now available. It currently has one subcommand, `watch`, a [ghcid](https://github.com/ndmitchell/ghcid)-style tool that runs clingo when .lp files change and formats the output.
+
+This is also a library for mapping clingo's output to pandas DataFrames. Check out the [examples](examples).
+
+```sh
+cd examples/map
+./map.py
 ```
 
 ## Tests
