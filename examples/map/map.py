d61109e05c9e499302c5caeb50d353067b707a2e
Darius Foo <darius.foo.tw@gmail.com>
2019-11-18 23:56:13 +0800

    Map column names

--- examples/map/map.py
+++ examples/map/map.py
@@ -8,8 +8,8 @@ world = solve(file='map.lp')
 g = Digraph()
 
 for wo in world:
-  [g.node(str(v._1), str(v._1)) for v in wo['vertex'].itertuples()]
-  [g.edge(str(e._1), str(e._2)) for e in wo['edge'].itertuples()]
+  [g.node(str(v.id), str(v.id)) for v in wo['vertex'].itertuples()]
+  [g.edge(str(e.a), str(e.b)) for e in wo['edge'].itertuples()]
 
   with open('/tmp/lol.dot', 'w') as f:
     f.write(g.source)
--- exnihilo/solve/__init__.py
+++ exnihilo/solve/__init__.py
@@ -3,6 +3,7 @@ import clyngor
 import itertools
 import pandas as pd
 from .interruptible import *
+import re
 
 program = '''
 a.
@@ -21,9 +22,9 @@ def process(answer):
   return {k: list(list(name_and_args[1]) for name_and_args in v) for k, v in itertools.groupby(sorted(answer, key=fst), key=fst)}
 
 
-def process_df(answer):
-  # TODO map names at this point
-  return {k: pd.DataFrame(name_and_args[1] for name_and_args in v) for k, v in itertools.groupby(sorted(answer, key=fst), key=fst)}
+def process_df(answer, mappings):
+  g = itertools.groupby(sorted(answer, key=fst), key=fst)
+  return {k: pd.DataFrame((name_and_args[1] for name_and_args in v), columns=mappings.get(k, [])) for k, v in g}
 
 def solve(program=None, file=None):
   if not program and not file:
@@ -33,6 +34,12 @@ def solve(program=None, file=None):
   if file:
     with open(file, 'r') as f:
       program = f.read()
+
+  mappings = {}
+  for m in re.finditer(r'%% (\w+)\(([^)]+)\)', program):
+    name = m.group(1)
+    args = re.split(r', ?', m.group(2))
+    mappings[name] = args
 
   # TODO handle unsat
   # TODO random
@@ -50,7 +57,7 @@ def solve(program=None, file=None):
   # assumes that there's no overloading by arity
 
   for answer in answers: # nondeterminism
-    yield process_df(answer)
+    yield process_df(answer, mappings)
 
 # for a in solve(program):
   # print(a)
